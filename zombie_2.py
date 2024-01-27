4b99fabbaec234630a32e80ad2b297f6ff9a5ee4
Rihaan B H <rihaan1810@gmail.com>
2024-01-27 12:42:03 +0530

    feat: tcpkill for zombie without pid

--- zombie_2.py
+++ zombie_2.py
@@ -2,7 +2,8 @@ import psutil
 import threading
 import os
 import signal
-import queue, subprocess
+import queue
+import subprocess
 
 p_z = []
 timers = []
@@ -12,8 +13,10 @@ kill_list = []
 def check_established_cases():
     pass
 
+
 def eliminate_without_pid():
     pass
+
 
 def check_probable_zombies():
     p_z.clear()
@@ -23,32 +26,45 @@ def check_probable_zombies():
             print("done")
         elif c.status == "ESTABLISHED":
             check_established_cases()
-        
 
     if not p_z:
         print("None")
 
+
 def set_timer():
     timers.clear()
-    
+
     for p in p_z:
         timers.append(threading.Timer(5.0, z_kill(p)))
+
 
 def execute():
     for timer in timers:
         timer.start()
 
+
 def z_kill(zombie):
     if zombie.status in ["FIN_WAIT2", "FIN_WAIT1", "CLOSE_WAIT", "LAST_ACK", "TIME_WAIT"]:
         kill_list.append(zombie)
 
+
 def kill(kill_list):
     for con in kill_list:
-        if con.pid == None:
-            subprocess.call(["sudo", "", ""]), 
-        else:
-            pid = str(con.pid)
-            subprocess.call(["sudo", "kill", "-9", pid])
+        if con.laddr.family == psutil.AF_INET and con.laddr.port == 80:
+
+            
+            if con.pid == None:
+                interface = con.laddr.ifname
+
+                interface_address = psutil.net_if_addrs()[interface][0].address
+
+                print("Interface name:", interface)
+                print("Interface address:", interface_address)
+                subprocess.call(["sudo", "tcpkill", "-i", "any", "port", con.laddr[1]])
+            else:
+                pid = str(con.pid)
+                subprocess.call(["sudo", "kill", "-9", pid])
+
 
 if __name__ == "__main__":
     check_probable_zombies()
